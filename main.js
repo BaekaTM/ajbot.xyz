b6fb320f4712b8f9b1e78e72295c6d32bdd3f3d2
MichLLLLL <66214943+MichLLLLL@users.noreply.github.com>
2021-07-01 16:19:25 +0200

    Update main.js

--- main.js
+++ main.js
@@ -12,3 +12,23 @@ document.onclick = (e) => {
         document.getElementById("nav-mobile").classList.remove("open")
     }
 }
+
+function updateServerCount() {
+    let servercount = document.getElementById("b-servercount-vrm");
+
+    let request = new XMLHttpRequest();
+    request.open("GET", 'https://top.gg/api/bots/727574553576079372/stats')
+    request.send()
+    request.onload = () => {
+        if (request.status === 200) {
+            let objetJS = JSON.parse(request.response);
+            let serverCount = objetJS.server_count;
+            servercount.innerHTML = serverCount.toLocaleString();
+        }
+        else {
+            servercount.innerHTML = "*something went wrong*";
+        }
+    }
+}
+
+updateServerCount()
